15c9769f417ce3723f3f89be404317b6f241162a
FabianGraml <fabian.graml03@gmail.com>
2023-12-02 17:18:46 +0100

    day2 complete

--- AdventOfCode/Day2.cs
+++ AdventOfCode/Day2.cs
@@ -1,17 +1,27 @@
-﻿namespace AdventOfCode;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
 public class Day2
 {
     public static void Main()
     {
-        string[] input = File.ReadAllLines("Input\\Day2\\day2_1.in");
-
-        int sumOfPossibleGameIds = CalculateSumOfPossibleGameIds(input);
+        // First half
+        string[] input1 = File.ReadAllLines("Input\\Day2\\day2_1.in");
+        int sumOfPossibleGameIds = CalculateSumOfPossibleGameIds(input1);
         Console.WriteLine("Sum of IDs of possible games: " + sumOfPossibleGameIds);
+
+        // Second half
+        string[] input2 = File.ReadAllLines("Input\\Day2\\day2_2.in");
+        List<CubeGame> games = ParseGames(input2);
+        int powerSums = games.Select(game => CalculateMinimumPower(game)).Sum();
+        Console.WriteLine($"Sum of powers: {powerSums}");
     }
-    static int CalculateSumOfPossibleGameIds(string[] input)
+
+    // For the first half
+    private static int CalculateSumOfPossibleGameIds(string[] input)
     {
         int sum = 0;
-        foreach (var line in input)
+        foreach (string line in input)
         {
             if (IsGamePossible(line))
             {
@@ -22,7 +32,7 @@ public class Day2
         return sum;
     }
 
-    static bool IsGamePossible(string gameData)
+    private static bool IsGamePossible(string gameData)
     {
         int maxRed = 12, maxGreen = 13, maxBlue = 14;
         string[] rounds = gameData.Split(';');
@@ -34,20 +44,22 @@ public class Day2
             foreach (string cube in cubes)
             {
                 string[] parts = cube.Trim().Split(' ');
-                if (parts.Length is not 2) continue;
+                if (parts.Length < 2) continue;
 
-                int count = int.Parse(parts[0]);
-                switch (parts[1])
+                if (int.TryParse(parts[0], out int count))
                 {
-                    case "red":
-                        red += count;
-                        break;
-                    case "green":
-                        green += count;
-                        break;
-                    case "blue":
-                        blue += count;
-                        break;
+                    switch (parts[1])
+                    {
+                        case "red":
+                            red += count;
+                            break;
+                        case "green":
+                            green += count;
+                            break;
+                        case "blue":
+                            blue += count;
+                            break;
+                    }
                 }
             }
 
@@ -56,9 +68,60 @@ public class Day2
         return true;
     }
 
-    static int ExtractGameId(string gameData)
+    private static int ExtractGameId(string gameData)
     {
         string[] parts = gameData.Split(':');
         return int.Parse(parts[0].Split(' ')[1]);
     }
+
+    // For the second half
+    private static List<CubeGame> ParseGames(string[] data)
+    {
+        List<CubeGame> games = new();
+        Regex colorPattern = new (@"(\d+) (red|green|blue)");
+
+        foreach (string line in data)
+        {
+            string[] parts = line.Split(':');
+            CubeGame game = new () { Id = int.Parse(parts[0].Split(' ')[1]) };
+
+            string[] sets = parts[1].Split(';');
+            foreach (string set in sets)
+            {
+                MatchCollection matches = colorPattern.Matches(set);
+                int red = 0, green = 0, blue = 0;
+                foreach (Match match in matches.Cast<Match>())
+                {
+                    int count = int.Parse(match.Groups[1].Value);
+                    switch (match.Groups[2].Value)
+                    {
+                        case "red": red += count; break;
+                        case "green": green += count; break;
+                        case "blue": blue += count; break;
+                    }
+                }
+                game.Sets.Add((red, green, blue));
+            }
+            games.Add(game);
+        }
+        return games;
+    }
+
+    private static int CalculateMinimumPower(CubeGame game)
+    {
+        int maxRed = 0, maxGreen = 0, maxBlue = 0;
+        foreach ((int Red, int Green, int Blue) in game.Sets)
+        {
+            maxRed = Math.Max(maxRed, Red);
+            maxGreen = Math.Max(maxGreen, Green);
+            maxBlue = Math.Max(maxBlue, Blue);
+        }
+        return maxRed * maxGreen * maxBlue;
+    }
+}
+
+public class CubeGame
+{
+    public int Id { get; set; }
+    public List<(int Red, int Green, int Blue)> Sets { get; set; } = new List<(int Red, int Green, int Blue)>();
 }
